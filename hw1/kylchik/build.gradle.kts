b55b1fd79a12f1dd083cd1444f8ea2e7a093792b
Ivan Kylchik <cilcic.v22@gmail.com>
2018-10-24 15:51:16 +0300

    Сделал первое домашнее задание

--- hw1/kylchik/build.gradle.kts
+++ hw1/kylchik/build.gradle.kts
@@ -5,11 +5,14 @@ plugins {
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
     testCompile("junit", "junit", "4.12")
     compile("com.sparkjava","spark-core","2.7.2")
+    compile("org.apache.spark","spark-core_2.11","2.3.2")
+
 }
 
 configure<JavaPluginConvention> {

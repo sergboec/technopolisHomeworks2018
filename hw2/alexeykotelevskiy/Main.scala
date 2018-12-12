3b7e2d97f246d5506228716dbc599b79e6990e07
Kotelevskiy Alexey <a_kotelevskiy@mail.ru>
2018-12-13 01:24:07 +0300

    hw1 removed

--- hw2/alexeykotelevskiy/Main.scala
+++ hw2/alexeykotelevskiy/Main.scala
@@ -2,7 +2,7 @@ package alexeykotelevskiy
 
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.LogisticRegression
-import org.apache.spark.ml.evaluation.{BinaryClassificationEvaluator, RegressionEvaluator}
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.tuning.{ParamGridBuilder, TrainValidationSplit}
 import org.apache.spark.sql.SparkSession
